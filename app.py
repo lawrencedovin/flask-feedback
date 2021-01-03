88ad9c4b984d83f77582443d6bff7b16478e045d
Lawrence Dovin <lawrencedovin@gmail.com>
2021-01-02 22:31:06 -0500

    Added register form wtform

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db, db, User
+from forms import RegisterForm
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///flask_feedback"
@@ -12,3 +13,10 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 connect_db(app)
 
 toolbar = DebugToolbarExtension(app)
+
+@app.route('/')
+def home_page():
+    return redirect('/register')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_user():
