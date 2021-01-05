d7e74f679d1ba825dab4984eab281567896641d6
Lawrence Dovin <lawrencedovin@gmail.com>
2021-01-05 15:06:20 -0500

    Added initial feedback flask form and feedback view page

--- forms.py
+++ forms.py
@@ -13,4 +13,9 @@ class RegisterForm(FlaskForm):
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+
+class FeedbackForm(FlaskForm):
+    title = StringField('Title', validators=[InputRequired()])
+    content = StringField('Content', validators=[InputRequired()])
+
     
